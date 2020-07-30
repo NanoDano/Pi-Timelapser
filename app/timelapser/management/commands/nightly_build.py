b6114db17abc007495865886372b336a250046a6
John Leon <nanodano@devdungeon.com>
2020-07-30 01:18:34 -0500

    More debug and fixing gzip

--- app/timelapser/management/commands/nightly_build.py
+++ app/timelapser/management/commands/nightly_build.py
@@ -53,7 +53,7 @@ class Command(BaseCommand):
 
     def zip_video(self):
         self.stdout.write(f'Zipping {self.video_path}')
-        gzip_result = system(f'gzip -f {self.video_path}'):
+        gzip_result = system(f'gzip -f {self.video_path}')
         if gzip_result != 0:
             self.stdout.write(self.style.ERROR(f'Error zipping {self.video_path} - Gzip result: {gzip_result}'))
             raise Exception(f'Error zipping {self.video_path}')
